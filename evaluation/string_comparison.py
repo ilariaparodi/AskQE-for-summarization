aa2bf5d614b4cffc4b6e69263b0011a2806f19df
Ilaria Parodi <146097375+ilariaparodi@users.noreply.github.com>
2026-02-13 18:36:32 +0100

    Update string_comparison.py

--- evaluation/string_comparison.py
+++ evaluation/string_comparison.py
@@ -54,10 +54,10 @@ def main():
 
             for pred, ref in zip(preds, refs):
 
-                if pred == "NOT_FOUND" and ref == "NOT_FOUND":
+                if pred == "No_Answer" and ref == "No_Answer":
                     continue
 
-                if pred == "NOT_FOUND" or ref == "NOT_FOUND":
+                if pred == "No_Answer" or ref == "No_Answer":
                     row_scores.append({
                         "f1": 0.0,
                         "em": False,
